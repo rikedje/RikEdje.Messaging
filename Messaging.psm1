f8cf7c816ea647595176732883b160c76d33ce2e
rikedje <rikard.edje@gmail.com>
2015-05-13 22:36:25 +0200

    Somewhat more robust

--- Messaging.psm1
+++ Messaging.psm1
@@ -3,5 +3,9 @@
 
  .Description
   Displays a visual representation of a calendar. This function supports multiple months
-  and lets you highlight specific date ranges or days. .Example # Send message to Slack $text = "Hello" $channel = "#general" $username = "Monkey-Bot" $emoji = ":monkey_face:" $uri = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX" Send-SlackMessage -Text $text -Channel $channel -UserName $username -Emoji $emoji -Uri $uri#> function Send-SlackMessage {    param(        $Text,        $Channel,        $UserName,        $Emoji,        $Uri    )        $data = 'payload={"text": "'+$Text+'" , "channel": "'+$Channel+'" , "username":"'+$Username+'", "icon_emoji": "'+$Emoji+'"}'
-        Invoke-WebRequest -Method Post -Body $data -Uri $Uri}Export-ModuleMember -Function Send-SlackMessage
+  and lets you highlight specific date ranges or days. .Example # Send message to Slack $text = "Hello" $channel = "#general" $username = "Monkey-Bot" $emoji = ":monkey_face:" $uri = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX" Send-SlackMessage -Text $text -Channel $channel -UserName $username -Emoji $emoji -Uri $uri#> function Send-SlackMessage {    param(        [string]$Text = "The Monkey-Bot says hi!",        [string]$Channel = "#general",        $UserName = "Monkey-Bot",        $Emoji = ":monkey_face:",        [Parameter(Mandatory=$true)]        $Uri    )
+    
+    # 'payload={"text": "The Monkey-Bot says hi!" , "channel": "#general" , "username":"Monkey-Bot", "icon_emoji": ":monkey_face:"}'
+    $data = New-Object PSObject -Property @{
+                text = $Text                channel = $Channel                username = $UserName                icon_emoji = $Emoji                uri = $Uri
+    }        $json = 'payload='+($data | ConvertTo-Json)    try {        Invoke-WebRequest -Method Post -Body $json -Uri $Uri | Out-Null    } catch {        "An error occured."        "Did you use a correct Uri?"        "Example: https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"    }}Export-ModuleMember -Function Send-SlackMessage
